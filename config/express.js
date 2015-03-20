ba5bd935dd3e192ddb1427068d976be44536d011
Ajit Pawar <me@pawar.ca>
2015-03-20 02:59:21 -0400

    node + sails + google auth working

---
+++ config/express.js
@@ -0,0 +1,77 @@
+// Google OAuth 2.0
+// https://github.com/jaredhanson/passport-google-oauth
+var passport = require('passport')
+    , GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+// Get credentials from
+// console.developers.google.com
+var GOOGLE_CLIENT_ID = "450642794440-5fs9j6a45oppub6rcbutthl3beb91t79.apps.googleusercontent.com";
+var GOOGLE_CLIENT_SECRET = "eUkL8EXspmvFUJ6W2H4kt0eL";
+var CALL_BACK_URL = "http://pawar.ca:3000/auth/google/callback";
+
+
+
+var verifyHandler = function(token, tokenSecret, profile, done) {
+  process.nextTick(function() {
+
+    User.findOne({uid: profile.id}, function(err, user) {
+      if (user) {
+        return done(null, user);
+      } else {
+
+        var data = {
+          provider: profile.provider,
+          uid: profile.id,
+          name: profile.displayName
+        };
+
+        if (profile.emails && profile.emails[0] && profile.emails[0].value) {
+          data.email = profile.emails[0].value;
+        }
+        if (profile.name && profile.name.givenName) {
+          data.firstname = profile.name.givenName;
+        }
+        if (profile.name && profile.name.familyName) {
+          data.lastname = profile.name.familyName;
+        }
+
+        User.create(data, function(err, user) {
+          return done(err, user);
+        });
+      }
+    });
+  });
+};
+
+passport.serializeUser(function(user, done) {
+  done(null, user.uid);
+});
+
+passport.deserializeUser(function(uid, done) {
+  User.findOne({uid: uid}, function(err, user) {
+    done(err, user);
+  });
+});
+
+/**
+ * Configure advanced options for the Express server inside of Sails.
+ *
+ * For more information on configuration, check out:
+ * http://sailsjs.org/#documentation
+ */
+module.exports.http = {
+
+  customMiddleware: function(app) {
+
+    passport.use(new GoogleStrategy({
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: CALL_BACK_URL
+    }, verifyHandler));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+  }
+
+};
+
---
+++ config/routes.js
@@ -0,0 +1,20 @@
+
+module.exports.routes = {
+
+  /* Note: according to policies/sessionAuth.js and config/policies.js,
+      if user session is not authenticated then they get redirected to "/login" */
+  '/' : {
+    controller : 'home'
+  },
+
+  '/login' : {
+    controller : 'auth',
+    action     : 'index'
+  },
+
+  '/logout' : {
+    controller : 'auth',
+    action     : 'logout'
+  }
+
+};
