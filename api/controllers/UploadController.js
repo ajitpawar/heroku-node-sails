7f2441e8f6ddaa9f8dcb2ef320833d100e549b17
Ajit Pawar <ajit@pawar.ca>
2015-04-19 06:01:44 +0000

    adding uploader scaffolding

---
+++ api/controllers/UploadController.js
@@ -0,0 +1,14 @@
+/**
+ * UploadController
+ *
+ * @description :: Server-side logic for managing uploads
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+  index: function (req, res) {
+    res.view();					// index.ejs by default
+  }
+};
+
